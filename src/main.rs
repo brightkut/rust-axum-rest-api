fe5441d6b479702dfd9eab4617c2ed7c6bc843f9
LonEr <dsorn2@gmail.com>
2025-06-18 09:36:07 +0700

    feat: add error custom

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::net::SocketAddr;
 
 use axum::{
-    Router,
+    Json, Router,
     extract::{Path, Query},
     http::Request,
     middleware,
@@ -9,6 +9,7 @@ use axum::{
     routing::{get, get_service},
 };
 use serde::Deserialize;
+use serde_json::json;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
 
@@ -62,9 +63,23 @@ async fn main_request_middleware<T>(req: Request<T>) -> Request<T> {
 
 async fn main_response_middleware(res: Response) -> Response {
     println!("Main response middleware");
-    println!();
 
-    res
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|e| e.client_status_and_error());
+
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let body = json!({
+                "error":{
+                    "type": client_error.as_ref()
+                }
+            });
+
+            (*status_code, Json(body)).into_response()
+        });
+
+    error_response.unwrap_or(res)
 }
 
 fn routes_static() -> Router {
